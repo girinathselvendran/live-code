989298dfd30c7f8e3de7fd5d34ddbe9503dcfeed
Girinath Selvendran <89913730+girinathselvendran@users.noreply.github.com>
2023-11-16 00:20:05 +0530

    changes

--- src/services/socketService.tsx
+++
@@ -1,7 +0,0 @@
-// src/services/socketService.js
-
-import { io } from "socket.io-client";
-
-const socket = io("http://localhost:5000"); // Replace with your Fastify server URL
-
-export default socket;
